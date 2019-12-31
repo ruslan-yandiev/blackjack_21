08f70eaef7baac9c0dace5fe47e1f8f8b946f6ac
ruslan <sqwer1986@gmail.com>
2019-12-31 11:17:57 +0300

    change class card

--- card.rb
+++ card.rb
@@ -1,21 +1,26 @@
+# class Card
 class Card
-  attr_reader :name
+  attr_reader :card
 
   SUITS =  %w[♠ ♥ ♣ ♦].freeze
   NAMES =  %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
   VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, [1, 11]].freeze
 
-  def initialize(name)
-    @name = name
+  def initialize(card)
+    @card = card
   end
 
-  def show
-    @name
+  def show_value
+    @card.values
+  end
+
+  def show_name
+    @card.keys
   end
 end
 
 Card::SUITS.each do |suit|
   Card::NAMES.each_with_index do |name, index|
-    Card.new(suit + name => Card::VALUES[index])
+   p Card.new(suit + name => Card::VALUES[index])
   end
 end
