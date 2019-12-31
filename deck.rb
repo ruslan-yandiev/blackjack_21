5280c60de2a776ae037a2682bf9eaf6525597cd2
ruslan <sqwer1986@gmail.com>
2019-12-31 09:16:10 +0300

    change class Deck

--- deck.rb
+++ deck.rb
@@ -1,17 +1,27 @@
 class Deck
+  @@cards = []
+
   def initialize
     @cards = []
   end
-
-  def add_card(card)
-    @cards << card
+  # добавляем карты в массив
+  def self.add_card(card)
+    @@cards << card
   end
-
+  # создает колоду карт у экземпляра
+  def add_card
+    @@cards.each { |card| @cards << card }
+  end
+  # возвращаем случайную карту из массива и при этом удаляем ее из массива
   def send_card
     @cards.delete_at(rand_index)
   end
 
   def rand_index
     rand(@cards.size)
+  end
+  # очищаем колоду экземпляра
+  def clear_deck
+    @cards.clear
   end
 end
