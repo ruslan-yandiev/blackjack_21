a09e4001b091323781a62ff2d8119e5d9eac23ec
ruslan <sqwer1986@gmail.com>
2020-01-05 23:26:05 +0300

    change class game

--- game.rb
+++ game.rb
@@ -11,33 +11,47 @@ class Game
     @bank += @player.bet
     @bank += @dealer.bet
   end
-
+  # метод анализирует количество очков у игроков и определяет победителя
   def analysis
-    game_overme if @player.money <= 0 || @dealer.money <= 0
-
-    if @player.points == @dealer.points
+    if @player.points == @dealer.points || @player.points > 21 && @dealer.points > 21
+      open_cards
       refund
-    elsif
+    elsif @player.points > @dealer.points && @player.points < 22
+      open_cards
+      @player.add_money(20)
+      restart_bank
+      game_overme?
+    elsif @dealer.points > @player.points && @dealer.points < 22
+      open_cards
+      @dealer.add_money(20)
+      restart_bank
+      game_overme?
     end
   end
   # метод возвращает ставки и вызывает метод обнуления банка
   def refund
+    puts 'Ничья'
     @player.add_money(10)
     @dealer.add_money(10)
     restart_bank
+  end
+
+  def open_cards
+    @player.show_cards!
+    @dealer.show_cards!
   end
 
   def restart_bank
     @bank = 0
   end
 
-  def game_overme
+  def game_overme?
     if @player.money <= 0
       puts 'Вы проиграли все деньги. Прощайте! '
-      break
+      true
     elsif @dealer.money <= 0
       puts 'Вы обчистили казино!'
-      break
+      true
     end
   end
 end
